8fe9b5e5e364430a9803af66e763bb7f3c061af1
bortek1993 <b.wojciechowski001@gmail.com>
2023-05-06 13:15:10 +0200

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        public string CheckResult(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            return number.ToString();
+           
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------FizzBuzz---------\nWitaj w grze podaj liczbę:");
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            while (true)
+            {
+                var number = GetValue();
+                var result = fizzBuzz.CheckResult(number);
+                Console.WriteLine(result);
+                Console.WriteLine("\nPodaj kolejną liczbę. Żeby zakończyć wybierz 't'");
+            }
+
+        }
+
+        private static int GetValue()
+        {
+            while (true)
+            {
+                var value = Console.ReadLine();
+                if (value.ToUpper() == "T")
+                    Environment.Exit(0);
+
+                if(!int.TryParse(value, out int number))
+                {
+                    Console.WriteLine("\nNiewłasćiwa wartość. Spróbuj ponownie. Żeby zakończyć wybierz 't'\n");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
